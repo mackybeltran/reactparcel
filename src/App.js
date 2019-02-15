10ef38d1d2bab86cbf372bb447ce956fd46fba69
Macky Beltran <macky@kearnstechnology.com>
2019-02-15 18:39:15 -0500

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter, Link, Switch, Router, Redirect} from 'react-router-dom';
 import './App.scss';
+import Home from './components/Home.js'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>
-            HELLO WORLD
-        </h1>
+    return <div className="App">
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+            </Switch>
+        </BrowserRouter>
+
       </div>
-    );
+
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './Home.scss';
+
+class Home extends PureComponent {
+
+    render() {
+        return <div className='Home'>
+            HELLO WORLD
+        </div>
+    }
+
+}
+
+export default Home
